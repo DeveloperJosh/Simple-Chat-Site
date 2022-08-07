ea9acb9928930daaaedd00503f2794aa4a32b79a
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-08-07 18:05:04 -0400

    Website

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,82 +4,62 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<div className="flex min-h-screen flex-col items-center justify-center py-2 bg-slate-700">
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+<Head>
+  <title>Create Next App</title>
+  <link rel="icon" href="/favicon.ico" />
+</Head>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+<main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+  <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 ">
+    <div className="flex justify-between items-center">
+      <div className="flex items-center">
+        <a href="/" className="text-3xl font-bold text-gray-900 dark:text-white">
+          Simple-Chat
         </a>
-      </footer>
+      </div>
+      <div className="flex items-center text-2xl">
+        <a href="/" className="text-gray-600 dark:text-gray-400">
+          Home
+        </a>
+        <a href="/" className="text-gray-600 dark:text-gray-400 ml-4">
+          About
+        </a>
+        <a href="/" className="text-gray-600 dark:text-gray-400 ml-4">
+          Setup
+        </a>
+      </div>
     </div>
+  </nav>
+  <h1 className="text-6xl font-bold text-white">
+    Welcome to{' '}
+    <a href="https://github.com/DeveloperJosh/Simple-Chat">
+      Simple-Chat
+    </a>
+  </h1>
+
+  <p className="mt-3 text-2xl text-white">
+    A simple chat app built with Nodejs and Socket.io
+  </p>
+
+
+  <div className="flex flex-col items-center justify-center mt-10">
+    <a href="/setup" className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+      Get Started
+    </a>
+  </div>
+
+</main>
+
+<footer className="flex h-24 w-full items-center justify-center text-white">
+  <a className="flex items-center justify-center gap-2" href="https://simple-chat.xyz/" target="_blank"
+    rel="noopener noreferrer">
+    Powered by Simple-Chat
+  </a>
+</footer>
+</div>
   )
 }
 
